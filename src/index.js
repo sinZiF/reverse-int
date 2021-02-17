0a06c2fb1ec3bc7d6b2c8eb3e810597804f201ef
Andrei Kisliuk <kisliuk.andrei@gmailcom>
2021-02-17 21:32:45 +0100

    feat: add the script to the index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
-  
+    let resolve, str, string, arr;
+    string = n.toString();
+    arr = string.split('');
+    
+    if (n > 0) {
+      if (arr[arr.length - 1] === 0) {
+          arr.pop()
+      }
+      str = arr.reverse().join('')
+      resolve = parseInt(str)
+      return resolve
+
+    } else if (n < 0 && arr.includes('-')){
+      arr.shift();
+      str = arr.reverse().join('');
+      resolve = parseInt(str)
+      return resolve
+    }
 }
